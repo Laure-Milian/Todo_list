6370ecf6132767e3c1477315857ba0ca9a603202
Juliane Blier <blier.juliane@gmail.com>
2016-10-11 12:29:25 +0200

    Correction bugs et Ajout de la sauvegarde des données dans le localStorage

--- app.js
+++ app.js
@@ -3,23 +3,33 @@
 	
 	//Création tableau vide
 	var tabTasks = [];
+	var data = JSON.parse(localStorage.getItem('data'));
+	if (data !== undefined && data !== null) {
+		tabTasks = data;
+		allTasks();
+	}
 
 	// Listener sur bouton add
 	$('#add').on('click', function() {
-		var indexTask = tabTasks.length;
-		tabTasks[indexTask] = {
-			title : $('input[name="todo"]').val(),
-			status : false
-		};
-		console.log(tabTasks);
+		if($('#fieldInput').val()){			
+			var indexTask = tabTasks.length;
+			tabTasks[indexTask] = {
+				title : $('input[name="todo"]').val(),
+				status : false
+			};
+			$('#fieldInput').val('');
+			console.log(tabTasks);
 
-		$('.tasks').append('<li><div class="ui checkbox"><input type="checkbox" data-index="'+ indexTask + '"><label data-index="'+ indexTask +'">'+ tabTasks[indexTask].title +'</label></div></li>');
+			$('.tasks').append('<li><div class="ui checkbox"><input type="checkbox" data-index="'+ indexTask + '"><label data-index="'+ indexTask +'">'+ tabTasks[indexTask].title +'</label></div></li>');
+			localStorage.setItem('data', JSON.stringify(tabTasks));
+		}
 	});
 
 	//Clic sur checkbox pour modifier class
 	$('.tasks').on('click', 'input[type="checkbox"]', function(){
 		var numInput = $(this).data('index');
 		isChecked(numInput);
+		localStorage.setItem('data', JSON.stringify(tabTasks));
 		
 	});
 
@@ -44,76 +54,43 @@
 	});
 
 	$('#allTasks').on('click', function() {
+		allTasks();
+	});
+
+
+
+
+	function isChecked(numInput){
+		if (!tabTasks[numInput].status){
+			$('label[data-index="'+ numInput +'"]').addClass('checked');
+			tabTasks[numInput].status = true;
+			console.log('addClass');
+		} else {
+			$('label[data-index="'+ numInput +'"]').removeClass('checked');
+			tabTasks[numInput].status = false;
+			console.log('removeClass');
+		}
+	}
+
+	function statusFalse(index){
+		$('.tasks').append('<li><div class="ui checkbox"><input type="checkbox" data-index="' + index + '"><label data-index="' + index +'">' + tabTasks[index].title + '</label></div></li>');
+	}
+
+	function statusTrue(index){
+		$('.tasks').append('<li><div class="ui checkbox"><input type="checkbox" checked="checked" data-index="' + index + '"><label data-index="' + index +'">' + tabTasks[index].title + '</label></div></li>');
+		$('label[data-index="'+ index +'"]').addClass('checked');
+	}
+
+	function allTasks(){
 		$('.tasks').html('');
 		var len = tabTasks.length;
 		for (var i = 0; i < len; i++) {
 			if (tabTasks[i].status) {
 				statusTrue(i);
 			}
-			else {
+			else if (!tabTasks[i].status){
 				statusFalse(i);
 			}
 		}
-	});
-
-
-
-
-function isChecked(numInput){
-	if (!tabTasks[numInput].status){
-			$('label[data-index="'+ numInput +'"]').addClass('checked');
-			tabTasks[numInput].status = true;
-			console.log('addClass');
-		} else{
-			$('label[data-index="'+ numInput +'"]').removeClass('checked');
-			tabTasks[numInput].status = false;
-			console.log('removeClass');
 	}
-}
-
-function statusFalse(index){
-	$('.tasks').append('<li><div class="ui checkbox"><input type="checkbox" data-index="' + index + '"><label data-index="' + index +'">' + tabTasks[index].title + '</label></div></li>');
-}
-
-function statusTrue(index){
-	$('.tasks').append('<li><div class="ui checkbox"><input type="checkbox" checked="checked" data-index="' + index + '"><label data-index="' + index +'">' + tabTasks[index].title + '</label></div></li>');
-	$('label[data-index="'+ index +'"]').addClass('checked');
-}
-
-
-	/*$('.tasks').append('<label><input type="checkbox">Sortir le chien</label>');
-
-
-		//Au click sur Ajouter, on ajoute la tache à la liste
-		$('#add').on('click', function(){
-			var newTask = $('input[name="todo"]').val();
-			var $label = $("label");
-			var next_num = $label.length + 1;
-			console.log('Next_num = ' + next_num);
-			$('.tasks').append('<label id="task' + next_num + '"><input data-num="' + next_num + '" type="checkbox">' + newTask + '</label>');
-
-
-			//Au click sur une checkbox, on active ou désactive la tache
-			$('input[type="checkbox"]').on('click', function(){
-				console.log('Au click sur la tache');
-				var numInput = $(this).data('num');
-				console.log("numInput" + numInput);
-				isChecked(numInput);
-			});
-
-		});
-
-		//Fonction qui active ou désactive la tache
-		function isChecked(numeroTask) {
-			if ($('input[data-num="'+ numeroTask +'"]').is(':checked')) {
-				$('#task' + numeroTask).addClass("checked");
-				console.log('checked donc Je change la classe');
-			} else {
-				$('#task' + numeroTask).removeClass("checked");
-				console.log('unchecked donc je change la classe');
-			}
-		}*/
-
-
-
-	})();
+})();
